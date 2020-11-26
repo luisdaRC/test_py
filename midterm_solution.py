19bf87b85b3bd5222ecb80ae3bdeb049c7edd87e
luisdaRC <luixdani123@gmail.com>
2020-11-26 17:28:47 -0500

    Agregando items

--- midterm_solution.py
+++ midterm_solution.py
@@ -71,6 +71,7 @@ def solucion():
 
 # Ejercicio 1: Promedio por cada semestre
     promedios = {}
+    lista = [0,0,0]
     for key,value in estudiantes.items():
         suma_creditos = 0
         suma_notas = 0
@@ -79,66 +80,22 @@ def solucion():
         for diccionario_interior in value:
             if diccionario_interior["Semestre"] == "2003-2P":
                 suma_creditos = suma_creditos + diccionario_interior["Creditos"]
-                promedios[key] = promedios[value] + diccionario_interior["Nota"]
+                lista[0] = lista[0] + diccionario_interior["Nota"]
 
             elif diccionario_interior["Semestre"] == "2004-1P":
                 suma_creditos = suma_creditos + diccionario_interior["Creditos"]
-                promedios[key] = promedios[value] + diccionario_interior["Nota"]
+                lista[0] = lista[0] + diccionario_interior["Nota"]
 
             elif diccionario_interior["Semestre"] == "2004-2P":
                 suma_creditos = suma_creditos + diccionario_interior["Creditos"]
-                suma_creditos = suma_creditos + diccionario_interior["Nota"]
+                lista[0] = lista[0] + diccionario_interior["Nota"]
 
-
-
-
-            for llave,valor in diccionario_interior.items():
-                if valor == "2003-2P":
-                    suma_notas =
-                elif valor == "2004-1P":
-
-                elif valor== "2003-2P":
 
                 sumatoria = value
 
 
 
 
-    goleadores_2014 = {"Muller": ("Alemania", 5),
-                       "Dempsey": ("USA", 2),
-                       "James": ("Colombia", 6),
-                       "Muller": ("Alemania", 5),
-                       "Schurrle": ("Alemania", 3),
-                       "Messi": ("Argentina", 4),
-                       "Suarez": ("Uruguay", 2),
-                       "van Persie": ("Holanda", 4),
-                       "Benzema": ("Francia", 3),
-                       "Klose": ("Alemania", 2),
-                       "Robben": ("Holanda", 3),
-                       "Valencia": ("Ecuador", 3),
-                       "Neymar": ("Brasil", 4),
-                       "Shaqiri": ("Suiza", 3),
-                       "Kroos": ("Alemania", 2),
-                       "Luiz": ("Brasil", 2)
-                       }
-
-    #Ejercicio 1-Test
-
-    mayor=0
-    for key, value in goleadores_2014.items():
-
-        if value[1]>mayor:
-            mayor=value[1]
-            nombre=key
-
-    print(nombre)
-
-"""
-Listas: Se puede hacer lo que quiera xD. Se define mi_lista = []
-Tuplas: No se pueden eliminar valores. Se define tuplita = ()   >>> b = (5,)  # Es una tupla <<< b = (5) es un int
-Conjuntos: No se pueden repetir valores. Se define: conjunto = set()
-Diccionarios: Key : Value. Se define diccionario = {}
-"""
 
 
 
